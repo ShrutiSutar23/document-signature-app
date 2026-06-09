f2be0a1d62331b92235743bd8f251b1016c95684
ShrutiSutar23 <shruti.sutar231992@gmail.com>
2026-06-09 23:15:51 +0530

    Day 2: Authentication system - register, login, JWT

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database import Base, engine
+from routers import auth
+
+# Create all tables in database
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Document Signature App", version="1.0.0")
 
@@ -10,6 +15,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Register routers
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
---
+++ backend/services/auth.py
@@ -0,0 +1,35 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import bcrypt
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def hash_password(password: str) -> str:
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(
+        plain_password.encode('utf-8'),
+        hashed_password.encode('utf-8')
+    )
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
